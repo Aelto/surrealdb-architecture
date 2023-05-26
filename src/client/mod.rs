e1555fe9aebee218788d12922011dad479185373
Aelto <Aelto@users.noreply.github.com>
2023-05-26 12:24:18 +0200

    use absolute path for DB address

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -5,7 +5,10 @@ use surrealdb::Surreal;
 use crate::errors::ApiResult;
 
 pub async fn connect(address: &str, namespace: &str, database: &str) -> ApiResult<()> {
-  DB.connect::<surrealdb::engine::local::File>(address)
+  if let Err(_) = std::fs::create_dir_all(address) {}
+  let absolute_path = dunce::canonicalize(address).expect("invalid path");
+
+  DB.connect::<surrealdb::engine::local::File>(absolute_path.as_path())
     .await?;
 
   DB.use_ns(namespace).use_db(database).await?;
